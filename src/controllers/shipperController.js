10973c033a019daf025418ce21ed5603aa3e44b3
phlopes <phlopes93@gmail.com>
2021-10-25 22:38:51 -0300

    create shipperController

---
+++ src/controllers/shipperController.js
@@ -0,0 +1,11 @@
+const shipperService = require('../services/shipperService');
+
+const createShipper = async (req, res) => {
+  const { name, doc, about, active, site } = req.body;
+  const shipper = await shipperService.createShipper(name, doc, about, active, site);
+  return res.status(201).json(shipper)
+}
+
+module.exports = {
+  createShipper
+}
