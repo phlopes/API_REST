74dd4edde8dc0c3c6c98059bc2c62ed506ff698d
phlopes <phlopes93@gmail.com>
2021-10-26 10:40:13 -0300

    route /transporter implementation to register transporters

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const shipperController = require('../controllers/shipperController');
+const transporterController = require('../controllers/transporterController');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/shipper', shipperController.createShipper);
 app.get('/shipper', shipperController.getShippers);
+app.post('/transporter', transporterController.createTransporter);
 
 module.exports = app;
--- src/models/transporterModel.js
+++ src/models/transporterModel.js
@@ -3,7 +3,7 @@ const connect = require('./connection');
 const createTransporter = async (name, doc, about, active, site) => {
   const db = await connect();
   const transporter = await db.collection('transporters').insertOne({ name, doc, about, active, site });
-  return {_id: shipper.insertedId, name, doc, about, site };
+  return {_id: transporter.insertedId, name, doc, about, site };
 };
 
 module.exports = {
