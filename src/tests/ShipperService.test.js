afcbd3bbc0c0523f1e0c8c761a8744ac0116ce4b
phlopes <phlopes93@gmail.com>
2021-10-31 18:21:04 -0300

    fix: indetation shipperService and TransporterService

--- src/tests/ShipperService.test.js
+++ src/tests/ShipperService.test.js
@@ -10,47 +10,46 @@ describe('Testando a camada ShipperService', () => {
     site: 'https://cliente01.com.br'
   };
 
+  describe('Quando algum campo não é preenchido, retorna uma mensagem de erro.', () => {
+    it('Quando o name não é preenchido', async () => {
+      const { doc, about, active, site } = payload;
+      const response = await ShipperService.createShipper({doc, about, active, site});
+      expect(response.code).to.be.equals(400);
+      expect(response.message).to.be.equals('"name" is required');
+    });
 
-describe('Quando algum campo não é preenchido, retorna uma mensagem de erro.', () => {
-  it('Quando o name não é preenchido', async () => {
-    const { doc, about, active, site } = payload;
-    const response = await ShipperService.createShipper({doc, about, active, site});
-    expect(response.code).to.be.equals(400);
-    expect(response.message).to.be.equals('"name" is required');
-  });
+    it('Quando o doc não é preenchido', async () => {
+      const { name, about, active, site } = payload;
+      const response = await ShipperService.createShipper({name, about, active, site});
+      expect(response.code).to.be.equals(400);
+      expect(response.message).to.be.equals('"doc" is required');
+    });
 
-  it('Quando o doc não é preenchido', async () => {
-    const { name, about, active, site } = payload;
-    const response = await ShipperService.createShipper({name, about, active, site});
-    expect(response.code).to.be.equals(400);
-    expect(response.message).to.be.equals('"doc" is required');
-  });
+    it('Quando o about não é preenchido', async () => {
+      const { name, doc, active, site } = payload;
+      const response = await ShipperService.createShipper({name, doc, active, site});
+      expect(response.code).to.be.equals(400);
+      expect(response.message).to.be.equals('"about" is required');
+    });
 
-  it('Quando o about não é preenchido', async () => {
-    const { name, doc, active, site } = payload;
-    const response = await ShipperService.createShipper({name, doc, active, site});
-    expect(response.code).to.be.equals(400);
-    expect(response.message).to.be.equals('"about" is required');
-  });
+    it('Quando o site não é preenchido', async () => {
+      const { name, doc, about, active } = payload;
+      const response = await ShipperService.createShipper({name, doc, about, active});
+      expect(response.code).to.be.equals(400);
+      expect(response.message).to.be.equals('"site" is required');
+    });
 
-  it('Quando o site não é preenchido', async () => {
-    const { name, doc, about, active } = payload;
-    const response = await ShipperService.createShipper({name, doc, about, active});
-    expect(response.code).to.be.equals(400);
-    expect(response.message).to.be.equals('"site" is required');
-  });
-
-describe('Quando um Expedidor/Cliente é cadastrado com sucesso', () => {
-  it('retorna um objeto', async() => {
-    const response = await ShipperService.createShipper(payload);
-    expect(response).to.be.a('object');
-  });
+  describe('Quando um Expedidor/Cliente é cadastrado com sucesso', () => {
+    it('retorna um objeto', async() => {
+      const response = await ShipperService.createShipper(payload);
+      expect(response).to.be.a('object');
+    });
   
-  it('esse objeto possui um id do novo Expedidor/Cliente inserido', async () => {
-    const response = await ShipperService.createShipper(payload);
-    const { shipper } = response;
-    expect(shipper).to.have.a.property('_id');
-  });  
-});
-});
+    it('esse objeto possui um id do novo Expedidor/Cliente inserido', async () => {
+      const response = await ShipperService.createShipper(payload);
+      const { shipper } = response;
+      expect(shipper).to.have.a.property('_id');
+    });  
+  });
+  });
 });
