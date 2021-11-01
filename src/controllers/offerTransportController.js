020791e800409bba23b5469d368d637c84787de4
phlopes <phlopes93@gmail.com>
2021-11-01 17:21:25 -0300

    fix: ajusting layers offerTransporters for tests

--- src/controllers/offerTransportController.js
+++ src/controllers/offerTransportController.js
@@ -3,7 +3,7 @@ const offerTransportService = require('../services/offerTransportService')
 const createOfferTransporter = async (req, res) => {
   const { id_transporter, id_offer, value, amount } = req.body;
 
-  const { code, message } = await offerTransportService.createOfferTransport(id_transporter, id_offer, value, amount);
+  const { code, message } = await offerTransportService.createOfferTransport({ id_transporter, id_offer, value, amount });
   if (message) return res.status(code).json({ message })
   
   return res.status(201).json({ message: 'transport offer registered successfully' });
